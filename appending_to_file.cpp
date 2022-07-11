97c610ddade51c27e7a55a9a9b2523708f93d759
Brandon Apol <brandonapol@cedarville.edu>
2022-07-10 20:44:57 -0400

    appending code updated

--- appending_to_file.cpp
+++ appending_to_file.cpp
@@ -1,23 +1,29 @@
 // Appends data to the end of file data.txt
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
 
 int main() 
     {
     using namespace std;
+    char in_file_name[16], out_file_name[16];
+    ifstream in_stream;
+    ofstream out_stream;
 
-    cout << "Opening data.txt for appending.\n";
-    ofstream fout;
-    fout.open("data.txt", ios::app);
-    if (fout.fail())
+    cout << "Name of file to read:\n";
+    cin >> in_file_name;
+    cout << "Opening " << in_file_name << " for appending.\n";
+    ofstream out_stream;
+    out_stream.open(in_file_name, ios::app);
+    if (out_stream.fail())
     {
         cout << "Input file opening failed.\n";
         exit(1);
     }
 
-    fout << " Haw\n";
+    out_stream << " Haw\n";
 
-    fout.close();
+    out_stream.close();
     cout << "End of appending to file.\n";
 
     return 0;
